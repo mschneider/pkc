05bf70cf36ee250c06df814249883d9eaf1b84f0
Maximilian Schneider <mail@maximilianschneider.net>
2022-03-18 22:50:03 +0100

    implement xsalsa20-poly1305 decryption

--- programs/pkc/src/lib.rs
+++ programs/pkc/src/lib.rs
@@ -4,77 +4,87 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod pkc {
-    use rsa::{
-        pkcs1::DecodeRsaPrivateKey, pkcs8::EncodePublicKey, BigUint, PaddingScheme, RsaPrivateKey,
-        RsaPublicKey,
-    };
-
-    use num_bigint::{IntoBigInt, ToBigInt};
-    use num_traits::sign::Signed;
-    use std::borrow::Cow;
 
     use super::*;
+    use std::convert::TryInto;
+
     pub fn initialize(
         ctx: Context<Initialize>,
-        public_key: String,
+        public_key: Vec<u8>,
         _seed: Vec<u8>,
         _bump: u8,
     ) -> ProgramResult {
         let acc = &mut ctx.accounts.encrypted_account;
-        acc.public_key = Some(public_key);
+        acc.public_key = public_key;
         Ok(())
     }
 
-    pub fn commit_value(ctx: Context<WithAccount>, encrypted_value: Vec<u8>) -> ProgramResult {
+    pub fn commit_value(
+        ctx: Context<WithAccount>,
+        nonce: Vec<u8>,
+        encrypted_value: Vec<u8>,
+    ) -> ProgramResult {
         let acc = &mut ctx.accounts.encrypted_account;
+        acc.nonce = nonce;
         acc.encrypted_value = encrypted_value;
         Ok(())
     }
 
-    pub fn publish_secret(ctx: Context<WithAccount>, secret_key: String) -> ProgramResult {
+    pub fn publish_secret(ctx: Context<WithAccount>, secret_key: Vec<u8>) -> ProgramResult {
         let acc = &mut ctx.accounts.encrypted_account;
-        acc.secret_key = Some(secret_key);
-
-        let mut secret_key =
-            RsaPrivateKey::from_pkcs1_pem(acc.secret_key.as_ref().unwrap()).unwrap();
-        secret_key.precompute();
-
-        let public_key = RsaPublicKey::from(secret_key.clone());
-        let pem = public_key.to_public_key_pem(Default::default()).unwrap();
-        assert_eq!(&pem, acc.public_key.as_ref().unwrap());
-
-        acc.cached_secret_key = bincode::serialize(&secret_key).unwrap();
-
-        Ok(())
-    }
-
-    pub fn prepare_decryption(ctx: Context<WithAccount>) -> ProgramResult {
-        let acc = &mut ctx.accounts.encrypted_account;
-
+        acc.secret_key = secret_key;
         Ok(())
     }
 
     pub fn reveal_value(ctx: Context<WithAccount>) -> ProgramResult {
         let acc = &mut ctx.accounts.encrypted_account;
-        let secret_key =
-            bincode::deserialize::<RsaPrivateKey>(acc.cached_secret_key.as_slice()).unwrap();
 
+        /*
+        use xsalsa20poly1305::{aead::Aead, Nonce};
+        let nonce = Nonce::from_slice(acc.nonce.as_slice());
+
+        let public_key_bytes: [u8; crypto_box::KEY_SIZE] =
+            acc.public_key.as_slice().try_into().unwrap();
+        let public_key = crypto_box::PublicKey::from(public_key_bytes);
+
+        let secret_key_bytes: [u8; crypto_box::KEY_SIZE] =
+            acc.secret_key.as_slice().try_into().unwrap();
+        let secret_key = crypto_box::SecretKey::from(secret_key_bytes);
+
+        let cypher = crypto_box::SalsaBox::new(&public_key, &secret_key);
         solana_program::log::sol_log_compute_units();
-        msg!("pre");
 
-        acc.decrypted_value = secret_key
-            .decrypt::<u64>(PaddingScheme::new_pkcs1v15_encrypt(), &acc.encrypted_value)
+        let plaintext = cypher
+            .decrypt(nonce, acc.encrypted_value.as_slice())
+            .unwrap();
+
+        */
+
+        use xsalsa20poly1305::aead::{Aead, NewAead};
+        use xsalsa20poly1305::{Key, Nonce, XSalsa20Poly1305};
+        let nonce = Nonce::from_slice(acc.nonce.as_slice()); // 24-bytes; unique
+        let key = Key::from_slice(acc.secret_key.as_slice());
+        let cypher = XSalsa20Poly1305::new(key);
+        let plaintext = cypher
+            .decrypt(nonce, acc.encrypted_value.as_slice())
             .unwrap();
 
         solana_program::log::sol_log_compute_units();
-        panic!("end");
+
+        msg!(
+            "plaintext={}",
+            std::str::from_utf8(plaintext.as_slice()).unwrap()
+        );
+
+        acc.decrypted_value = plaintext;
+        // panic!("post");
 
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-#[instruction(_public_key: String, seed: Vec<u8>, bump: u8)]
+#[instruction(_public_key: Vec<u8>, seed: Vec<u8>, bump: u8)]
 
 pub struct Initialize<'info> {
     pub payer: Signer<'info>,
@@ -100,11 +110,9 @@ pub struct WithAccount<'info> {
 #[account]
 #[derive(Default)]
 pub struct EncryptedAccount {
-    pub public_key: Option<String>,
-    pub secret_key: Option<String>,
+    pub nonce: Vec<u8>,
+    pub public_key: Vec<u8>,
+    pub secret_key: Vec<u8>,
     pub encrypted_value: Vec<u8>,
     pub decrypted_value: Vec<u8>,
-
-    // performance optimization
-    pub cached_secret_key: Vec<u8>,
 }
